eef4bb77e6ed7b419a5ab1e28db9445ad99b8a9f
Jarl Evanson <jarl.evanson@herronhighschool.org>
2023-02-14 20:46:42 -0500

    Added function for setting output, fixed naming scheme

--- tests/test.rs
+++ tests/test.rs
@@ -1,7 +1,7 @@
 #[test]
 fn compile_bin() {
-    let mut instance = nasm::Instance::new("tests/test.s");
-    instance.set_format(nasm::OutputFormat::Binary);
+    let mut instance = nasm_build::Instance::new("tests/test.s");
+    instance.set_format(nasm_build::OutputFormat::Binary);
     let output_filepath = instance.compile().unwrap();
 
     println!("{}", &output_filepath.display());
